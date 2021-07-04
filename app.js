f1e5ab9c363f60f22528794c7b8d04650c31e611
Luis Martinez <luis-martinez@users.noreply.github.com>
2021-07-04 10:14:43 -0700

    Computer plays

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log('Hello world');
+
+const options = ['Rock', 'Paper', 'Scissors'];
+
+// Computer plays, return a random value either 'Rock', 'Paper', or 'Scissors'.
+function computerPlay(){
+  return options[Math.floor(Math.random()*options.length)];
+}
+
+console.log(computerPlay());
